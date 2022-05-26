e3e4c319d9f188142a848d09519dd563aacfc1e1
Name <name@email.com>
2022-05-26 15:55:26 +0530

    Added Database tables and migrations.

---
+++ Data/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LMS.Data
+{
+    public class LeaveType: BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+
+
+    }
+}
